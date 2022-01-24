b4a8c5fbbeb8c89ef7d23a532b877a4a338f5e07
Peter Irvine <peirvine@gmail.com>
2022-01-24 10:16:46 -0600

    yarn and basic updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Katie and Trygve are getting Married!
         </p>
         <a
           className="App-link"
