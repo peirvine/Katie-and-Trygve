97ec121a8666a4119459c050101051eb7daf937e
Peter Irvine <peirvine@gmail.com>
2022-02-05 17:06:06 -0600

    mobile mvp

--- src/compnents/home/Home.jsx
+++ src/compnents/home/Home.jsx
@@ -12,10 +12,16 @@ const payload = {
 export default function Home () {
   return ( 
     <div id="Home">
+      <div id="mobileHeader">
+        <h1>Katie + Trygve</h1>
+        <h2>JUNE 25, 2022 • DELLWOOD, MN</h2>
+      </div>
       <img src={homeCollage} alt="a collage of katie and trygve" />
       <Split {...payload} />
-      <h2>Wedding Day</h2>
-      <h3> June 25, 2022</h3>
+      <div id="desktopDate">
+        <h2>Wedding Day</h2>
+        <h3> June 25, 2022</h3>
+      </div>
     </div>
    )
 }
